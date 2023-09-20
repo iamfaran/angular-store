101e6d956721f425e047082f60801621dd190ba9
Faran Javed <faran1997@outlook.com>
2023-09-20 18:07:16 +0500

    add products array

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import { products } from '../products';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
-
+  products = [...products];
+  constructor() {
+    console.log(this);
+  }
 }
