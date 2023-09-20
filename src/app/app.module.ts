ed9bd0551a884aaddf1864ee5edd67ae8731e720
Faran Javed <faran1997@outlook.com>
2023-09-20 17:00:47 +0500

    add product list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
-  declarations: [AppComponent, TopBarComponent],
+  declarations: [AppComponent, TopBarComponent, ProductListComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
