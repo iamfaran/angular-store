8215b23f712f92ac26038ab93ff75b2f9fdd32ad
Faran Javed <faran1997@outlook.com>
2023-09-26 16:17:17 +0500

    update cart count

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { CartService } from '../cart.service';
+import { Product } from '../products';
 
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.css'],
 })
-export class TopBarComponent {}
+export class TopBarComponent {
+  cartItems = this.cartService.items;
+  constructor(private cartService: CartService) {
+    console.log(this);
+  }
+}
